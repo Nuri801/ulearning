5f6d2465091b0f7c98626321c503936b9680548f
nuri <nuriddin@utec.kr>
2023-12-13 10:45:23 +0900

    Extract common widgets.

--- lib/welcome/welcome.dart
+++ lib/welcome/welcome.dart
@@ -5,7 +5,6 @@ class Welcome extends StatelessWidget {
   const Welcome({super.key});
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Stack(
         children: [
