1566c2246bf25e4ee11f9e28e9868252820fe1fc
nuri <nuriddin@utec.kr>
2024-03-07 09:27:46 +0900

    Riverpod state controller update.

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -12,7 +12,6 @@ class SignIn extends ConsumerWidget {
   final TextEditingController idController = TextEditingController();
   final TextEditingController pwController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final regProvider = ref.watch(registerNotifierProvider);
